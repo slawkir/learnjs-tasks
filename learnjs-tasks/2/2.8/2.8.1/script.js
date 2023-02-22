a017542c5173cd5acd6454814d379e0b06e67430
slawkir <slawkir1984@gmail.com>
2023-02-22 23:37:07 +0400

    feat: add solution of tasks 2.8 basic operators

---
+++ learnjs-tasks/2/2.8/2.8.1/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let a = 1;
+let b = 1;
+
+let c = ++a; // c = 2
+let d = b++; // d = 1
+
+// a = 2
+// b = 2
---
+++ learnjs-tasks/2/2.8/2.8.2/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let a = 2;
+let x = 1 + (a *= 2);
+
+// a = 4;
+// x = 5;
