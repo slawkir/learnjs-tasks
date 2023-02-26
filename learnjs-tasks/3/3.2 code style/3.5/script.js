fe839ed3b01596c2e01f7f6632077c15f111d106
slawkir <slawkir1984@gmail.com>
2023-02-26 18:56:00 +0400

    add solution tasks 3.5

---
+++ learnjs-tasks/3/3.2 code style/3.5/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+it("Возводит x в степень n", function() {
+  let x = 5;
+
+  let result = x;
+  assert.equal(pow(x, 1), result);
+
+  result *= x;
+  assert.equal(pow(x, 2), result);
+
+  result *= x;
+  assert.equal(pow(x, 3), result);
+});
+
+//лучше разбить на блоков it
+
+ it("5 в степени 1 будет 5", function() {
+    assert.equal(pow(5, 1), 5);
+  });
+
+  it("5 в степени 2 будет 25", function() {
+    assert.equal(pow(5, 2), 25);
+  });
+
+  it("5 в степени 3 будет 125", function() {
+    assert.equal(pow(5, 3), 125);
+  });
