66b21e6223b3e7963225dffd7fcaf9c41c97020d
slawkir <slawkir1984@gmail.com>
2023-04-09 21:56:44 +0400

    add solution tasks Document 7.3

---
+++ learnjs-tasks/Document/3.2/script.js
@@ -0,0 +1,7 @@
+let elem = document.getElementById('elem');
+
+function clear(element) { 
+  element.innerHTML = '';
+}
+
+clear(elem);
--- learnjs-tasks/Document/script.js
+++ learnjs-tasks/Document/script.js
@@ -1,1 +1,4 @@
-alert(Array.from(document.body.childNodes));
+let div2 = div.cloneNode(true);
+div2.querySelector('strong').innerHTML = 'Всем пока!';
+
+div.after(div2);
